709303ad55ea3bbd3d989994fd38bd36b11bc69d
Hemwatie Persaud <hemwatiepersaud@gmail.com>
2021-10-09 20:26:12 -0400

    before added all kubernetes demo app yaml files

--- src/components/views/SidebarClusterView.tsx
+++ src/components/views/SidebarClusterView.tsx
@@ -4,13 +4,16 @@ import { useEffect, useRef, useCallback, useState } from "react";
 function SidebarClusterView(props: any) {
     const containerRef = React.useRef<HTMLDivElement>(null);
 
-    
+
 
     return(
         <div style={ { display:'inline-block', float: 'left',  width: '300px', height: '600px'  }}>
-            <h1>
-                sidebar
-            </h1>
+            <h1>Control Plane</h1>
+            <table>
+                <tr>
+                    <td> </td>
+                </tr>
+            </table>
         </div>
     )
 }
