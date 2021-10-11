e0a023365924f1bee5bd9c623d466636ff4bc049
Brian Kang <brkang1@gmail.com>
2021-10-11 16:02:59 -0400

    add new test

---
+++ __tests__/schedulerLogs.test.ts
@@ -0,0 +1,8 @@
+import * as thing from '../server/schedulerLogs';
+
+describe("test schedulerLogs command", () => {
+  thing.getDeploymentNames();
+  // it('should create a file called SomeFile.txt', () => {
+
+  // });
+});
