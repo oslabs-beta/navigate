7184748c0aa6d5ea3d0f235a3b05b5bd85d9d7b4
Hemwatie Persaud <hemwatiepersaud@gmail.com>
2021-10-12 17:13:55 -0400

    fix yaml parser test

--- __tests__/YAMLParser.test.ts
+++ __tests__/YAMLParser.test.ts
@@ -1,13 +1,13 @@
 import getYAMLData from "../server/yamlParser";
 
-xdescribe("unit tests for YAML parser", () =>{
-  const data: any = getYAMLData();
+describe("unit tests for YAML parser", () =>{
+  const data: any = getYAMLData()[0];
 
   it("should return a JSON file that has an array", () => {
     expect((data)).toBeInstanceOf(Array);
   });
 
   it("should return a JSON with k8s-specific fields", () => {
-    expect(data[0]["kind"] && data[0]["api"]).toBeTruthy();
+    expect(data[0].kind && data[0].apiVersion).toBeTruthy();
   });
 });
