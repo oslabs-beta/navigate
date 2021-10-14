18a30e54c9b86eeb683a08771ad6f8ff07ffb056
Hemwatie Persaud <hemwatiepersaud@gmail.com>
2021-10-14 13:38:40 -0400

    created scrollable display for statusConditions array, removed legend which  breaks node view

--- src/components/views/SidebarClusterView.tsx
+++ src/components/views/SidebarClusterView.tsx
@@ -4,39 +4,26 @@ import { useEffect, useRef, useCallback, useState } from "react";
 function SidebarClusterView(props: any) {
   const containerRef = React.useRef<HTMLDivElement>(null);
   let deploymentStatus = props.deploymentStatus;
+  console.log('inside sidebar: ', deploymentStatus);
+
+  const statuses: any[] = [];
+  deploymentStatus.forEach((ele: any) => {
+    statuses.push(<div style={{borderBottom:"thin solid gray", borderTop:"thin solid gray"}}>
+      <p  style={{textAlign:"right"}} >{ele.kind}</p><p>Name: {ele.name}</p><p>Event: {ele.event}</p><p>{ele.time}</p>
+      </div>)
+  })
+
 
   return props.namespace !== "Kubernetes Cluster" ? (
     <div>
       <h1>{props.masterNode}</h1>
-      <div id="sidebar">
-      <div id="legend">
-          <h2>Legend</h2>
-            <p>◇ = Kubernetes Cluster</p>
-            <p>○ = Deployment</p>
-            <p>▭ = Namespace</p>
-            <p>△  = Service</p>
-            <p>▱  = Stateful Set</p>
-            <p style={{color:"light gray"}}>--- = Uses</p>
-            <p style={{color:"purple"}}>--- = Deploys</p>
-            <p style={{color:"orange"}}>⇢ = Deploys/Uses</p>
-      </div>
-    </div>
     </div>
   ) : (
     <div>
-    <h1>{props.namespace}</h1>
-    <div id="sidebar">
-      <div id="legend">
-          <h2>Legend</h2>
-            <p>◇ = Kubernetes Cluster</p>
-            <p>○ = Deployment</p>
-            <p>▭ = Namespace</p>
-            <p>△  = Service</p>
-            <p>▱  = Stateful Set</p>
-            <p style={{color:"light gray"}}>--- = Uses</p>
-            <p style={{color:"purple"}}>--- = Deploys</p>
-            <p style={{color:"orange"}}>⇢ = Deploys/Uses</p>
-      </div>
+        <h1>{props.namespace}</h1>
+        <h2>Node Scheduler</h2>
+    <div id='statusBox' style={{width: '300px', height:'600px' , overflow: 'scroll'}}>
+        {statuses}
     </div>
     </div>
   );
