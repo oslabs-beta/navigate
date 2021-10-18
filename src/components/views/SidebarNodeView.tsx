59c1b31219913f240bca21ea6e1b8f5b3ad3a0e0
Hemwatie Persaud <hemwatiepersaud@gmail.com>
2021-10-18 00:27:43 -0400

    fixed table errors for displaying deployment information in SidebarNodeView

--- src/components/views/SidebarNodeView.tsx
+++ src/components/views/SidebarNodeView.tsx
@@ -1,38 +1,31 @@
 import * as React from "react";
-import { useEffect, useRef, useCallback, useState } from "react";
+import kDeploymentLive from '../../../server/kDeploymentLive';
 
 function SidebarNodeView(props: any) {
   console.log('master node is: ', props.masterNode)
   console.log('inside nodeView sidebar: ', props.podDeployments)
   const podDeployObjs = props.podDeployments;
 
-  const deploymentMain = [];
-  podDeployObjs.forEach((ele: object)=>{
+  const deploymentMain: any = [];
+  podDeployObjs.forEach((ele: kDeploymentLive) => {
     if(ele.name === props.masterNode){
       deploymentMain.push(
         <div>
-          <table> 
+          <table>
             <tbody>
-              <tr>Name: <td>{ele.name}</td></tr>
-              <tr>Kind: <td>{ele.kind}</td></tr>
-              <tr>DNS Policy: <td>{ele.dnsPolicy}</td></tr>
-              <tr>Environment Variables: 
-                <td><table><tbody>
-                  <td>Name<tr>{ele.env[0].name}</tr></td>
-                  <td>Value<tr>{ele.env[0].value}</tr></td>
-                </tbody>
-                </table></td>
-                </tr>
-              <tr>Strategy:<td>{ele.strategy.type}</td></tr>
-              <tr>{ele.strategy.type}:
-                <td><table><tbody>
-                  <td>Max Surge:<tr>{ele.strategy.rollingUpdate.maxSurge}</tr></td>
-                  <td>Max Unavailable:<tr>{ele.strategy.rollingUpdate.maxUnavailable}</tr></td>
-                  </tbody></table></td>
-              </tr>
-              <tr>UID: <td>{ele.uid}</td></tr>
+              <tr><td>Name:</td><td>{ele.name}</td></tr>
+              <tr><td>Kind:</td><td>{ele.kind}</td></tr>
+              <tr><td>Namespace:</td><td>{ele.namespace}</td></tr>
+              <tr><td>Created at:</td><td>{ele.created}</td></tr>
+              <tr><td>Resource Version:</td><td>{ele.resourceVersion}</td></tr>
+              <tr><td>Restart Policy:</td><td>{ele.restartPolicy}</td></tr>
+              <tr><td>Strategy Type:</td><td>{ele.strategyType}</td></tr>
+              <tr><td>Max Surge:</td><td>{ele.rollingUpdateMaxSurge}</td></tr>
+              <tr><td>Max Unavailable:</td><td>{ele.rollingUpdateMaxUnavailable}</td></tr>
+              <tr><td>Scheduler Name:</td><td>{ele.schedulerName}</td></tr>
+              <tr><td>UID:</td><td>{ele.uid}</td></tr>
             </tbody>
-            </table>
+          </table>
         </div>
       )
     }
