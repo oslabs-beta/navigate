1f4efbda19ffd8d76b6c199d24a702579593ed51
Hemwatie Persaud <hemwatie.persaud@gmail.com>
2021-10-26 17:31:47 -0400

    changed console log

--- src/components/views/FetchLiveData.tsx
+++ src/components/views/FetchLiveData.tsx
@@ -5,7 +5,7 @@ function FetchLiveData(props: any) {
   const reRunGetKubernetesData = () => {
     function requestUpdate() {
         fetch('http://localhost:3000/update')
-          .then(response => console.log("Successful reRunGetKubernetesData"))
+          .then(response => console.log("Successful"))
           .catch(error => console.log('POST ERROR: ' + error));
       }
     requestUpdate();
