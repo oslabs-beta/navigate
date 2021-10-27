bfe7c538056fbf4eb72e593cf0e6b2d2757c118e
bk <brkang1@gmail.com>
2021-10-27 18:25:26 -0400

    add component tests

--- __tests__/SidebarClusterView.test.tsx
+++ __tests__/SidebarClusterView.test.tsx
@@ -4,7 +4,28 @@ import '@testing-library/jest-dom';
 import SidebarClusterView from '../src/components/views/SidebarClusterView';
 
 test('<SidebarClusterView/>', async () => {
+  //rerender original component with props to be called when needed
+  const { rerender } = render(<SidebarClusterView deploymentStatus={mockProps} />);
+
   render(<SidebarClusterView />)
-  //should have a button due to FetchLiveData component
-  expect(screen.getByRole('button')).toBeTruthy();
+  //should have a button called "refresh" due to FetchLiveData component
+  expect(screen.getAllByText('refresh')).toBeTruthy();
+  //it should not display any events if props are empty
+  expect(screen.getByText('Kubernetes engine not running, no nodes detected.'));
+  //should display events if props have something
+  rerender;
+  expect(screen.getByText('Kind:'));
 });
+
+const mockProps = [
+  {
+  deploymentStatus: [
+    {
+      Kind: "NetworkPolicy",
+      Name: "app-v1",
+      Event: "Mocked",
+      Time: "now"
+    }
+  ]
+  }
+];
--- src/components/views/SidebarClusterView.tsx
+++ src/components/views/SidebarClusterView.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
 import { useEffect, useRef, useCallback, useState } from "react";
 import NotLiveMessage from "./NotLiveMessage";
+import {anyObject} from '../../kObjects/__index'
+
+interface IProps{
+  deploymentStatus?: anyObject[],
+  namespace?: string
+}
 
 import FetchLiveData from "./FetchLiveData";
-function SidebarClusterView(props: any) {
+function SidebarClusterView(props: IProps) {
   const containerRef = React.useRef<HTMLDivElement>(null);
   let deploymentStatus = props.deploymentStatus;
 
   const statuses: any[] = [];
+  if(deploymentStatus)
   deploymentStatus.forEach((ele: any, index: number) => {
     statuses.push(<div style={{width:"300px", borderBottom:"thin solid gray", borderTop:"thin solid gray"}} key={index}>
       <table style={{width:"300px"}}>
@@ -20,7 +27,7 @@ function SidebarClusterView(props: any) {
       </table>
       </div>)
   })
-  if(statuses[0] === undefined) statuses.push(<NotLiveMessage />)
+  if(statuses[0] === undefined) statuses.push(<NotLiveMessage key={0}/>)
 
   return (
     <div>
