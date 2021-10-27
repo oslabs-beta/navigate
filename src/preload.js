b7818724b94b13b5c87b74277c6d63036fa520d7
Hemwatie Persaud <hemwatie.persaud@gmail.com>
2021-10-27 16:03:53 -0400

    modified preload.js to run server.ts

--- src/preload.js
+++ src/preload.js
@@ -1,6 +1,4 @@
-const {spawn} = require('child_process');
-// var exec = require('child_process').exec, child;
-const { execFile } = require('child_process');
+var exec = require('child_process').exec, child;
 const fs = require('fs');
 const path = require('path');
 
@@ -10,37 +8,21 @@ window.addEventListener('DOMContentLoaded', () => {
     const serverFile = path.join(__dirname, '../server/server.ts')
 
     runCommand = async (cmd) => {
-      // if (process.env.NODE_ENV === 'production') {
-      //   const fixPath = require('fix-path');
-      
-      //   fixPath();
-      // import {shellPath} from 'shell-path';
       console.log(process.env.SHELL);
-      //=> '/usr/bin'
-      // console.log(await shellPath());
-      //=> '/usr/local/bin:/usr/bin:...'
-      // exec(cmd, function (error, stdout, stderr) {
-      //     if(process.env.NODE_ENV !== 'test')
-      //     {
-      //         console.log(stdout);
-      //         if(stderr)
-      //         console.log('stderr: ' + stderr);
-      //         if (error !== null) {
-      //              console.log('exec error: ' + error);
-      //         }
-      //     }
-      // });
-
-      execFile('npx', ['ts-node', ''], (error, stdout, stderr) => {
-        if (error) {
-          throw error;
-        }
-        console.log(stdout);
+      await exec(cmd, function (error, stdout, stderr) {
+          if(process.env.NODE_ENV !== 'test')
+          {
+              console.log(stdout);
+              if(stderr)
+              console.log('stderr: ' + stderr);
+              if (error !== null) {
+                   console.log('exec error: ' + error);
+              }
+          }
       });
 
   }
-  // const serverFile = `${process.env.PWD}/server/server.ts`
-  // console.log(__dirname)
-  // runCommand(`ls`)
+
+  runCommand(`npx ts-node ${serverFile}`)
   
   })
