82205e24cb5ecd1dc92e71bcb6f65d5bf878f1c5
bk <brkang1@gmail.com>
2021-10-28 11:25:42 -0400

    put labels back to no background + whitesmoke text

--- src/components/views/Legend.tsx
+++ src/components/views/Legend.tsx
@@ -7,12 +7,11 @@ export default function Legend(props: any) {
       <tbody>
       <tr><th><h3 className="legendTitle">Legend</h3></th></tr>
       <tr><td><img src="https://i.ibb.co/s6YrbyT/round-Rect-Dark.png"></img></td><td>Namespace</td><td><img src="https://i.ibb.co/XXjPChx/podicon2.png" height="100%" width="100%"></img></td><td>Deployment</td></tr>
-      <tr><td><img src="https://i.ibb.co/rFcjXmq/round-Rect-Purple.png"></img></td><td>Stateful Set</td><td><img src="https://i.ibb.co/X5wCv7S/network-Icon.png"></img></td><td>Service</td></tr>
+      <tr><td><img src="https://i.ibb.co/rFcjXmq/round-Rect-Purple.png"></img></td><td style={{color:'orange'}}>⇢ </td><td style={{color:'orange'}}> Deploys/Uses</td>
       <tr></tr>
       <tr></tr>
-      <tr><td style={{color:'orange'}}>⇢ </td><td style={{color:'orange'}}> Deploys/Uses</td><td style={{color:"light gray"}}>---</td><td style={{color:"light gray"}}>Uses</td></tr>
+      <tr><td>Stateful Set</td><td><img src="https://i.ibb.co/X5wCv7S/network-Icon.png"></img></td><td>Service</td></tr><td style={{color:"light gray"}}>---</td><td style={{color:"light gray"}}>Uses</td></tr>
       <tr></tr>
-      
       <tr></tr>
       <tr><td>⬡</td><td>Control Plane</td></tr>
       </tbody>
@@ -30,7 +29,6 @@ export default function Legend(props: any) {
       <tr></tr>
       <tr><td style={{color:'orange'}}>⇢ </td><td style={{color:'orange'}}> Deploys/Uses</td><td style={{color:"light gray"}}>---</td><td style={{color:"light gray"}}>Uses</td></tr>
       <tr></tr>
-      
       <tr></tr>
       <tr><td style={{color:"purple"}}>---</td><td style={{color:"purple"}}>Deploys</td></tr>
       </tbody>
