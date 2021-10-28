8236531d5fb79f2cb13053d535cce807a37241eb
Hemwatie Persaud <hemwatie.persaud@gmail.com>
2021-10-28 18:01:59 -0400

    removed server loading for presentation

--- src/preload.js
+++ src/preload.js
@@ -5,24 +5,24 @@ const path = require('path');
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-    const serverFile = path.join(__dirname, '../server/server.ts')
+//     const serverFile = path.join(__dirname, '../server/server.ts')
 
-    runCommand = async (cmd) => {
-      console.log(process.env.SHELL);
-      await exec(cmd, function (error, stdout, stderr) {
-          if(process.env.NODE_ENV !== 'test')
-          {
-              console.log(stdout);
-              if(stderr)
-              console.log('stderr: ' + stderr);
-              if (error !== null) {
-                   console.log('exec error: ' + error);
-              }
-          }
-      });
+//     runCommand = async (cmd) => {
+//       console.log(process.env.SHELL);
+//       await exec(cmd, function (error, stdout, stderr) {
+//           if(process.env.NODE_ENV !== 'test')
+//           {
+//               console.log(stdout);
+//               if(stderr)
+//               console.log('stderr: ' + stderr);
+//               if (error !== null) {
+//                    console.log('exec error: ' + error);
+//               }
+//           }
+//       });
 
-  }
+//   }
 
-  runCommand(`npx ts-node ${serverFile}`)
+//   runCommand(`npx ts-node ${serverFile}`)
   
   })
